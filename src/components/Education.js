9748c568435f65ef4c857d0e9c23769b18c6f225
hemantanaxa-ui <hemanta.naxa@gmail.com>
2026-02-10 16:04:23 +0545

    Add Education component displaying degree and research focus

---
+++ src/components/Education.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <section>
+            <h2>Education</h2>
+            <ul>
+                <li>
+                    <strong>Degree:</strong> Bachelor of Science in Computer Science
+                </li>
+                <li>
+                    <strong>Research Focus:</strong> Artificial Intelligence and Machine Learning
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default Education;
